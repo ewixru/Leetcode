a305972f8efdf2cebad10fbfc06916adeaf34bfd
ewix <fpa.orel@gmail.com>
2024-05-20 15:14:19 +0300

    Solved 1480. Running Sum of 1D Array

---
+++ src/running-sum-of-1d-array/RunningSumBenchmark.cs
@@ -0,0 +1,49 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace running_sum_of_1d_array;
+public class RunningSumBenchmark
+{
+    [Benchmark]
+    public int[] RunningSumDefault() //input array moved inside the function for benchmark operation
+    {
+        int[] nums = [1, 2, 3, 4, 1, 1, 1, 1, 1, 3, 1, 2, 10, 1];
+        int[] sum = new int[nums.Length];
+        int lastSum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            lastSum = nums[i] + lastSum;
+            sum[i] = lastSum;
+        }
+        return sum;
+    }
+    [Benchmark]
+    public int[] RunningSumWithoutTempArray() //input array moved inside the function for benchmark operation
+    {
+        int[] nums = [1, 2, 3, 4, 1, 1, 1, 1, 1, 3, 1, 2, 10, 1];
+        for (int i = 1; i < nums.Length; i++)
+        {
+            nums[i] = nums[i - 1] + nums[i];
+        }
+        return nums;
+    }
+
+    #region Solution
+    /// <summary>
+    /// Method of problem solving for leetcode
+    /// </summary>
+    /// <param name="nums">array of integer values</param>
+    /// <returns></returns>
+    public int[] RunningSum(int[] nums)
+    {
+        int[] sum = new int[nums.Length];
+        int lastSum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            lastSum = nums[i] + lastSum;
+            sum[i] = lastSum;
+        }
+        return sum;
+    }
+    #endregion
+}
+
