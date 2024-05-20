f3185e9f64c1588ae8114f804cfdf7131bf24b67
ewix <fpa.orel@gmail.com>
2024-05-20 20:23:57 +0300

    Add 876. Middle of The Linked List solution

---
+++ src/Leetcode.Tests/MiddleOfTheLinkedListTest.cs
@@ -0,0 +1,54 @@
+﻿namespace Leetcode.Tests;
+
+public class MiddleOfTheLinkedListTest
+{
+    [Fact]
+    public void FirstExampleTest()
+    {
+        //WTF IS THAT INPUT
+        ListNode head = 
+            new(1,
+            new(2, 
+            new(3, 
+            new(4, 
+            new(5
+            )))));
+
+        MiddleOfTheLinkedList middleOfTheLinkedList = new();
+
+        string expectedValues = "345";
+        string result = "";
+        ListNode? currentNode = middleOfTheLinkedList.MiddleNode(head);
+        while (currentNode != null)
+        {
+            result += currentNode.val;
+            currentNode = currentNode.next;
+        }
+        Assert.Equal(expectedValues, result);
+    }
+    [Fact]
+    public void SecondExampleTest()
+    {
+        //WTF IS THAT INPUT
+        ListNode head =
+            new(1,
+            new(2,
+            new(3,
+            new(4,
+            new(5,
+            new(6
+            ))))));
+
+        MiddleOfTheLinkedList middleOfTheLinkedList = new();
+
+        string expectedValues = "456";
+        string result = "";
+        ListNode? currentNode = middleOfTheLinkedList.MiddleNode(head);
+        while (currentNode != null)
+        {
+            result += currentNode.val;
+            currentNode = currentNode.next;
+        }
+        Assert.Equal(expectedValues, result);
+    }
+}
---
+++ src/Leetcode/876. Middle of The Linked List/MiddleOfTheLinkedList.cs
@@ -0,0 +1,24 @@
+﻿namespace Leetcode;
+
+public class MiddleOfTheLinkedList
+{
+    public ListNode? MiddleNode(ListNode head)
+    {
+        int nodesCount = 0;
+        ListNode? currentNode = head;
+        while (currentNode?.next != null) 
+        {
+            currentNode = currentNode?.next;
+            nodesCount++;
+        }
+        int targetNode = (nodesCount / 2) + (nodesCount % 2);
+        nodesCount = 0;
+        currentNode = head;
+        while (nodesCount != targetNode)
+        {
+            currentNode = currentNode?.next;
+            nodesCount++;
+        }
+        return currentNode;
+    }
+}
