520e7fdce3f3010b7a4048d47102cd8ff62746e0
ewix <fpa.orel@gmail.com>
2024-05-20 23:35:18 +0300

    Add 383. Ransom Note solution

---
+++ src/Leetcode.Tests/RansomNoteTest.cs
@@ -0,0 +1,35 @@
+﻿namespace Leetcode.Tests;
+
+public class RansomNoteTest
+{
+    [Fact]
+    public void FirstExampleTest()
+    {
+        string ransomNoteInput = "a";
+        string magazine = "b";
+        bool expected = false;
+        RansomNote ransomNote = new();
+        bool result = ransomNote.CanConstruct(ransomNoteInput, magazine);
+        Assert.Equal(expected, result);
+    }
+    [Fact]
+    public void SecondExampleTest()
+    {
+        string ransomNoteInput = "aa";
+        string magazine = "ab";
+        bool expected = false;
+        RansomNote ransomNote = new();
+        bool result = ransomNote.CanConstruct(ransomNoteInput, magazine);
+        Assert.Equal(expected, result);
+    }
+    [Fact]
+    public void ThirdExampleTest()
+    {
+        string ransomNoteInput = "aa";
+        string magazine = "aab";
+        bool expected = true;
+        RansomNote ransomNote = new();
+        bool result = ransomNote.CanConstruct(ransomNoteInput, magazine);
+        Assert.Equal(expected, result);
+    }
+}
---
+++ src/Leetcode/383. Ransom Note/RansomNote.cs
@@ -0,0 +1,15 @@
+﻿namespace Leetcode;
+
+public class RansomNote
+{
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+        List<char> magazineChars = [.. magazine];
+        foreach (char c in ransomNote)
+        {
+            if (!magazineChars.Contains(c)) return false;
+            magazineChars.Remove(c);
+        }
+        return true;
+    }
+}
