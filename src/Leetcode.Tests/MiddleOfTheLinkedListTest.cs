54026620876902b66cc992e80c285e8b1b0f7a27
ewix <fpa.orel@gmail.com>
2024-05-20 23:04:26 +0300

    Add 876. Middle of The Linked List solution

--- src/Leetcode.Tests/MiddleOfTheLinkedListTest.cs
+++ src/Leetcode.Tests/MiddleOfTheLinkedListTest.cs
@@ -5,14 +5,8 @@ public class MiddleOfTheLinkedListTest
     [Fact]
     public void FirstExampleTest()
     {
-        //WTF IS THAT INPUT
-        ListNode head = 
-            new(1,
-            new(2, 
-            new(3, 
-            new(4, 
-            new(5
-            )))));
+        ListNode head = RecursiveFill(
+            new ListNode(5));
 
         MiddleOfTheLinkedList middleOfTheLinkedList = new();
 
@@ -29,15 +23,8 @@ public class MiddleOfTheLinkedListTest
     [Fact]
     public void SecondExampleTest()
     {
-        //WTF IS THAT INPUT
-        ListNode head =
-            new(1,
-            new(2,
-            new(3,
-            new(4,
-            new(5,
-            new(6
-            ))))));
+        ListNode head = RecursiveFill(
+            new ListNode(6));      
 
         MiddleOfTheLinkedList middleOfTheLinkedList = new();
 
@@ -50,5 +37,29 @@ public class MiddleOfTheLinkedListTest
             currentNode = currentNode.next;
         }
         Assert.Equal(expectedValues, result);
+    }
+    [Fact]
+    public void ThirdExampleTest()
+    {
+        ListNode head = new ListNode(1);
+
+        MiddleOfTheLinkedList middleOfTheLinkedList = new();
+
+        string expectedValues = "1";
+        string result = "";
+        ListNode? currentNode = middleOfTheLinkedList.MiddleNode(head);
+        while (currentNode != null)
+        {
+            result += currentNode.val;
+            currentNode = currentNode.next;
+        }
+        Assert.Equal(expectedValues, result);
+    }
+
+    public ListNode RecursiveFill(ListNode parent)
+    {
+        if (parent.val - 1 != 0)
+            return RecursiveFill(new ListNode(parent.val - 1, parent));
+        else return parent ?? throw new NullReferenceException();
     }
 }
--- src/Leetcode/876. Middle of The Linked List/MiddleOfTheLinkedList.cs
+++ src/Leetcode/876. Middle of The Linked List/MiddleOfTheLinkedList.cs
@@ -2,23 +2,44 @@
 
 public class MiddleOfTheLinkedList
 {
+    //public ListNode? MiddleNode(ListNode head)
+    //{
+    //    int nodesCount = 0;
+    //    ListNode? currentNode = head;
+    //    while (currentNode?.next != null) 
+    //    {
+    //        currentNode = currentNode?.next;
+    //        nodesCount++;
+    //    }
+    //    int targetNode = (nodesCount / 2) + (nodesCount % 2);
+    //    nodesCount = 0;
+    //    currentNode = head;
+    //    while (nodesCount != targetNode)
+    //    {
+    //        currentNode = currentNode?.next;
+    //        nodesCount++;
+    //    }
+    //    return currentNode;
+    //}
+
+    /// <summary>
+    /// Probably it's cheese solution 
+    /// </summary>
+    /// <param name="head">First node of linked list</param>
+    /// <returns>Middle node</returns>
     public ListNode? MiddleNode(ListNode head)
     {
-        int nodesCount = 0;
-        ListNode? currentNode = head;
-        while (currentNode?.next != null) 
+        ListNode currentNode = head;
+        List<ListNode> list = [];
+        while (true)
         {
-            currentNode = currentNode?.next;
-            nodesCount++;
+            list.Add(currentNode);
+            if(currentNode.next == null)
+            {
+                int targetNode = list.Count / 2;
+                return list[targetNode];
+            }
+            currentNode = currentNode.next;
         }
-        int targetNode = (nodesCount / 2) + (nodesCount % 2);
-        nodesCount = 0;
-        currentNode = head;
-        while (nodesCount != targetNode)
-        {
-            currentNode = currentNode?.next;
-            nodesCount++;
-        }
-        return currentNode;
     }
 }
